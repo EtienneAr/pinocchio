1b979e7109546477415c1a75a6502854dd663756
Megane Millan <megane.millan@inria.fr>
2024-09-29 14:31:57 +0200

    Add assert to non supported algorithms

--- include/pinocchio/algorithm/rnea-second-order-derivatives.hxx
+++ include/pinocchio/algorithm/rnea-second-order-derivatives.hxx
@@ -48,6 +48,11 @@ namespace pinocchio
       const Eigen::MatrixBase<TangentVectorType1> & v,
       const Eigen::MatrixBase<TangentVectorType2> & a)
     {
+      assert(
+        (std::is_same<JointModel, JointModelMimicTpl<Scalar, Options, JointCollectionTpl>>::value
+         == false)
+        && "Algorithm not supported for mimic joints");
+
       typedef typename Model::JointIndex JointIndex;
       typedef typename Data::Motion Motion;
       typedef typename Data::Inertia Inertia;
@@ -161,6 +166,10 @@ namespace pinocchio
       const Tensor3 & dtau_dqdv,
       const Tensor3 & dtau_dadq)
     {
+      assert(
+        (std::is_same<JointModel, JointModelMimicTpl<Scalar, Options, JointCollectionTpl>>::value
+         == false)
+        && "Algorithm not supported for mimic joints");
       typedef typename Data::Motion Motion;
       typedef typename Data::Force Force;
       typedef typename Data::Inertia Inertia;
